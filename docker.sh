2115e9622b00756a06371c76f74503716a8d8371
Nirmaljeet <500060993@stu.upes.ac.in>
2020-01-29 01:30:55 +0530

    This is my second commit

--- docker.sh
+++ docker.sh
@@ -18,3 +18,12 @@ docker tag b0e616c28473 nirmal0110/demorepo
 docker push nirmal0110/demorepo:1.0
 docker pull nirmal0110/demorepo:1.0
  
+git init
+git add Docker
+git add Dockerfile
+git add docker.sh
+git commit -m "This is my second commit"
+git remote add origin http://github.com/Nirmaljeet/Jenkins_pipeline_using_Microservices_and_Docker
+git push -u origin master
+echo "Nirmaljeet"
+echo "23062003vV"
